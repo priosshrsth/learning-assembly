5275d7dcbdf1c28700d004dcca2bfacc4542e728
priosshrsth <shrsthprios@hotmail.com>
2026-01-11 03:55:31 +0000

    initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  headers: () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ];
+  },
+  allowedDevOrigins: ["*.ngrok.free.app"],
 };
 
 export default nextConfig;
